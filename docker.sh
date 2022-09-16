2a5e1223fdda607a9084aba4eec40bc6fee580e2
20020314 <yitianyangtwo@qq.com>
2022-09-16 20:58:37 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -2,10 +2,8 @@
 curl -sSL https://get.docker.com | sh
 systemctl start docker
 systemctl enable docker
-#
 apt-get install docker-compose -y
 curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-#
 chmod a+x /usr/local/bin/docker-compose
 rm -rf `which dc`
 ln -s /usr/local/bin/docker-compose /usr/bin/dc
